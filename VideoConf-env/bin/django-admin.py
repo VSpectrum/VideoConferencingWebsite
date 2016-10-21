ea64d2ef5146d70676de57fab85bf37c0ca3d7bf
root <root@ubuntu.members.linode.com>
2016-10-20 23:36:41 -0400

    Added env

---
+++ VideoConf-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/var/www/PyDev/VidConf/VideoConferencingWebsite/VideoConf-env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
